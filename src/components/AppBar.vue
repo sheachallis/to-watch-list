e703a04cf715e0c7a4d3c80d367e198915654bcc
Shea Challis <shea.challis@hotmail.co.uk>
2023-02-19 20:37:54 +0000

    Added theme colours and icons

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,11 +1,15 @@
 <template>
-  <v-app-bar title="Work In Progress">
+  <v-app-bar>
+    <v-card class="mx-4 px-1 text-h6" color="primary" @click="onClick">
+      <span class="anton">MyMDb</span>
+    </v-card>
     <v-spacer />
     <settings-menu />
   </v-app-bar>
 </template>
 
 <script lang="ts">
+import { injectStore } from "@/stores";
 import { defineComponent } from "vue";
 import SettingsMenu from "./SettingsMenu.vue";
 
@@ -15,6 +19,17 @@ export default defineComponent({
   name: "AppBar",
   components: {
     SettingsMenu,
+  },
+  setup() {
+    const { routerStore } = injectStore();
+
+    function onClick() {
+      routerStore.push("/");
+    }
+
+    return {
+      onClick,
+    };
   },
 });
 </script>
--- src/components/ListCard.vue
+++ src/components/ListCard.vue
@@ -67,7 +67,7 @@ export default defineComponent({
   name: "ListsCard",
   setup() {
     const { notificationsStore, watchListStore } = injectStore();
-    const { notifyInfo, notifyError } = notificationsStore;
+    const { notifySuccess, notifyError } = notificationsStore;
     const { title, content, deleteItem, moveItem } = watchListStore;
 
     const state = reactive<State>({
@@ -80,7 +80,7 @@ export default defineComponent({
     function onShare() {
       try {
         shareStatus(unref(title), unref(content), () =>
-          notifyInfo("List '" + unref(title) + "' copied to clipboard", 2500)
+          notifySuccess("List '" + unref(title) + "' copied to clipboard")
         );
       } catch (e) {
         const error = e as Error;
